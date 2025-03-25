cddec975b0a2c5e35f34b93c9502da62bba95248
AN <drgoncom8@gmail.com>
2025-03-25 16:30:51 +0200

    My portfolio v1

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./assets/Fonts.css";
 import "./styles/assets/normalize.css";
 import "./index.css";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter} from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
