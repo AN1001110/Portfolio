ea6c91951b73a0b72a94f732283c85c1cfe255be
AN <drgoncom8@gmail.com>
2025-03-26 16:57:26 +0200

    My portfolio v1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,69 @@
 import { Link } from "react-router";
 import "../styles/components/Header.css";
-import { faBriefcase, faHouse, faPhone ,faAddressCard} from "@fortawesome/free-solid-svg-icons";
+import {
+  faBriefcase,
+  faHouse,
+  faPhone,
+  faAddressCard,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setScreenSize(window.innerWidth);
+    });
+  }, [screenSize]);
+  const [show, setShow] = useState(true);
   return (
     <header>
       <div className="container">
-        <Link to= "/" className="logo"> AN</Link>
-        <ul>
-          <li>
-            <Link to="/">
-            <FontAwesomeIcon  icon={faHouse} />
-              <span>Home</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/about">
-            <FontAwesomeIcon  icon={faAddressCard} />
-              <span>About Us</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/projects">
-            <FontAwesomeIcon icon={faBriefcase} />
-              <span>Projects</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact">
-            <FontAwesomeIcon  icon={faPhone} />
-              <span>Contact</span>
-            </Link>
-          </li>
-        </ul>
+        <Link to="/" className="logo">
+          AN
+        </Link>
+        <div>
+          <div
+            className="bar"
+            onClick={() => {
+              setShow((ele) => !ele);
+            }}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+          <ul
+            style={{
+              display: show ? "flex" : screenSize < 768 ? "none" : "flex",
+            }}
+          >
+            <li>
+              <Link to="/">
+                <FontAwesomeIcon icon={faHouse} />
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">
+                <FontAwesomeIcon icon={faAddressCard} />
+                <span>About Us</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/projects">
+                <FontAwesomeIcon icon={faBriefcase} />
+                <span>Projects</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact">
+                <FontAwesomeIcon icon={faPhone} />
+                <span>Contact</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
